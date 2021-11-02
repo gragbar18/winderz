db32fa2afb149f12e23695bc601765be6252f45f
Algonight <benjamin.fournet99@gmail.com>
2021-11-02 13:35:30 +0100

    Git test

--- app/src/main/java/com/via/android/winderzproject/MainActivity.java
+++ app/src/main/java/com/via/android/winderzproject/MainActivity.java
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button button;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

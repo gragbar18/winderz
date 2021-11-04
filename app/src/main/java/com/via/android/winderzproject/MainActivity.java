9febc8ddacafbde16abefc5a440c3a91879446e0
arthur-pouli <poulignyarthur1313@gmail.com>
2021-11-04 11:24:57 +0100

    Bottom bar layout
    Creat the main layout with the bottom bar

--- app/src/main/java/com/via/android/winderzproject/MainActivity.java
+++ app/src/main/java/com/via/android/winderzproject/MainActivity.java
@@ -3,18 +3,19 @@ package com.via.android.winderzproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    Button button;
-    TextView textView;
-    TextView textView2;
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigationView=findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setBackground(null);
     }
 }

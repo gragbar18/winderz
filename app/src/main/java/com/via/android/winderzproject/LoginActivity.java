20e75a1fdf6f0edd59d787c12bd57044c030c9f5
Algonight <benjamin.fournet99@gmail.com>
2021-11-07 18:51:12 +0100

    Navigation development

--- app/src/main/java/com/via/android/winderzproject/LoginActivity.java
+++ app/src/main/java/com/via/android/winderzproject/LoginActivity.java
@@ -1,6 +1,25 @@
 package com.via.android.winderzproject;
 
-import android.app.Activity;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class LoginActivity extends Activity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    Button buttonToHomeActivity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        buttonToHomeActivity = findViewById(R.id.buttonToHomeActivity);
+
+        buttonToHomeActivity.setOnClickListener(view -> {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        });
+    }
 }
--- app/src/main/java/com/via/android/winderzproject/MainActivity.java
+++ app/src/main/java/com/via/android/winderzproject/MainActivity.java
@@ -1,6 +1,10 @@
 package com.via.android.winderzproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
@@ -15,7 +19,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         bottomNavigationView=findViewById(R.id.bottomNavigationView);
         bottomNavigationView.setBackground(null);
+
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.homeFragment, R.id.mapFragment, R.id.addSessionFragment, R.id.favoriteFragment, R.id.profileFragment)
+                .build();
+
+
+        NavController navController = Navigation.findNavController(this, R.id.fragmentContainerView);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
 }
